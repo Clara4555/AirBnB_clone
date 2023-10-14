ec9bf687b7260bb89cc00cb5f9a0442356123d05
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-10-14 03:16:26 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,24 @@ import models
 
 class BaseModel():
     '''A class that defines all common attributes/methods for other classes'''
-    def __init__(self):
-        '''Initializing the Basemodel.'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        '''Initializing the Basemodel.
+        Args:
+        *args: won’t be used
+        **kwargs: dic representation of instance
+        '''
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''Returns the string representation of the BaseModel instance'''
